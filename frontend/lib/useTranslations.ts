1d89146f7a804b38c0951322a142dbca3cd2dad5
mxxx222 <max@herbspot.fi>
2025-10-23 08:04:36 +0300

    Remove all TypeScript files to use pure JavaScript build for Render deployment

--- frontend/lib/useTranslations.ts
+++
@@ -1,9 +0,0 @@
-import fi from "@/locales/fi.json";
-import en from "@/locales/en.json";
-import ru from "@/locales/ru.json";
-
-const dict = { fi, en, ru };
-
-export function useTranslations(lang: string) {
-  return dict[lang as keyof typeof dict] || fi;
-}
--- frontend/modules/ocr/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as OCRDropzone } from "./OCRDropzone";
-export { default as OCRHotkeys } from "./OCRHotkeys";
-export { default as OCRPreview } from "./OCRPreview";
-export { default as OCRRecent } from "./OCRRecent";
